0ebb62788f6390d94ed465ebac861e19ca020d83
Stephen Nuttall <stephenln99@gmail.com>
2025-11-18 19:47:26 -0500

    Workflow Test

--- WebApp/vite.config.js
+++ WebApp/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "ModTracker"
+    base: "ModTracker",
+    define: {
+        'import.meta.env.BACKEND_IP': JSON.stringify(process.env.BACKEND_IP),
+    },
 })
